af847ff8126e1f7e86453598df2a5a1f2d407eb1
Rahul Dhakad <dhakad706@gmail.com>
2023-12-22 22:55:59 +0530

    added database and responsiveness

--- app.js
+++ app.js
@@ -5,50 +5,62 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const data =require(__dirname+"/data.js")
 var lodash = require('lodash');
+const mongoose = require("mongoose");
+const dotenv =require("dotenv").config();
 
 
 const app = express();
-app.set("view engine", "ejs");
+app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
+mongoose.connect(process.env.MONGO_URI)
+
+const schema={
+  title: String,
+  post: String
+}
+const Blogs = mongoose.model("Blog",schema);
+
 
 
 let allPost=[];
+
+const getPosts= async ()=>{
+    const res = await Blogs.find({});
+    allPost=[...res]
+    // console.log(res)
+   
+}
+
+
+
 let homeStartingContent=data.homeStartingContent;
 let aboutContent=data.aboutContent;
 let contactContent=data.contactContent;
-
+ 
 
 
 app.get("/", (req, res) => {
-  console.log(allPost)
-  res.render("home", {
-    homeContent: homeStartingContent,
-    allPost:allPost
-  });
+  
+  getPosts();
+  setTimeout(() => {
+    res.render("home", {
+      homeContent: homeStartingContent,
+      allPost:allPost
+    });
+  }, 100);
+  
 });
 
 
-app.get("/posts/:para",(req,res)=>{
-  const prm=lodash.lowerCase(req.params.para);
-  console.log(prm);
-  
-  allPost.forEach((elem)=>{
-    const title=lodash.lowerCase(elem.title);
-    if(title===prm){
-      res.render("post",{title:elem.title,post:elem.post});
-    }
-    
-  })
-  
 
-})
 
 app.get("/about", (req, res) => {
   res.render("about", { aboutContent: aboutContent });
 });
+
 app.get("/contact", (req, res) => {
   res.render("contact", { contactContent: contactContent });
 });
@@ -57,6 +69,19 @@ app.get("/compose", (req, res) => {
   res.render("compose");
 });
 
+app.get("/:para",(req,res)=>{
+  const prm=lodash.lowerCase(req.params.para);
+  // console.log(prm);
+    
+  allPost.forEach((elem)=>{
+    const title=lodash.lowerCase(elem.title);
+    if(title===prm){
+      res.render("post",{title:elem.title,post:elem.post});
+    }
+    
+  })
+
+})
 
 // post requests
 
@@ -65,6 +90,9 @@ app.post("/compose",(req,res)=>{
     title:req.body.title,
     post:req.body.post
   };
+
+  const newBlog= new Blogs(newPost);
+  newBlog.save();
   allPost.push(newPost);
 
   res.redirect("/");
@@ -73,6 +101,8 @@ app.post("/compose",(req,res)=>{
 
 
 //listening
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+app.listen(3000,"localhost", function () {
+  console.log("Server started at http://localhost:3000");
 });
+
+//192.168.1.82
