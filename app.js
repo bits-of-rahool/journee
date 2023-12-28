4afa1735ebfa07b56ebd8f37c9378e63be4effea
Rahul Dhakad <dhakad706@gmail.com>
2023-12-28 06:52:17 +0000

    refactor: Update app.js and package.json
    Update import statements, mongoose connection, and package.json scripts.

--- app.js
+++ app.js
@@ -2,52 +2,46 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const data =require(__dirname+"/data.js")
-var lodash = require('lodash');
+const data = require(__dirname + "/data.js");
+var lodash = require("lodash");
 const mongoose = require("mongoose");
-const dotenv =require("dotenv").config();
-
+const dotenv = require("dotenv").config();
 
 const app = express();
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI);
 
+const schema = { title: String, post: String };
+const Blogs = mongoose.model("Blog", schema);
 
-const schema={title: String,post: String};
-const Blogs = mongoose.model("Blog",schema);
-
-
-var homeStartingContent=data.homeStartingContent;
-let aboutContent=data.aboutContent;
-let contactContent=data.contactContent;
-var allPost=[];
+var homeStartingContent = data.homeStartingContent;
+let aboutContent = data.aboutContent;
+let contactContent = data.contactContent;
+var allPost = [];
 
 //getting posts from the database.
-const getPosts = async function(callback)  {
+const getPosts = async function (callback) {
   try {
-      const res = await Blogs.find({});
-       allPost = [...res];
+    const res = await Blogs.find({});
+    allPost = [...res];
   } catch (error) {
-      console.error("Error fetching posts:", error);
+    console.error("Error fetching posts:", error);
   }
   callback();
 };
 
-
-
-
 //get requests
 app.get("/", (req, res) => {
-  const render=function (){
+  const render = function () {
     res.render("home", {
       homeContent: homeStartingContent,
-      allPost:allPost
+      allPost: allPost,
     });
-  }
-  getPosts(render)
-    //render function as callback
+  };
+  getPosts(render);
+  //render function as callback
 });
 
 app.get("/about", (req, res) => {
@@ -62,30 +56,29 @@ app.get("/compose", (req, res) => {
   res.render("compose");
 });
 
-app.get("/:para",(req,res)=>{
-  const prm=lodash.lowerCase(req.params.para);
-  allPost.forEach((elem)=>{
-    const title=lodash.lowerCase(elem.title);
-    if(title===prm){
-      res.render("post",{title:elem.title,post:elem.post});
-}})})
-
+app.get("/:para", (req, res) => {
+  const prm = lodash.lowerCase(req.params.para);
+  allPost.forEach((elem) => {
+    const title = lodash.lowerCase(elem.title);
+    if (title === prm) {
+      res.render("post", { title: elem.title, post: elem.post });
+    }
+  });
+});
 
 // post requests
-app.post("/compose",(req,res)=>{
-  const newPost={
-    title:req.body.title,
-    post:req.body.post
+app.post("/compose", (req, res) => {
+  const newPost = {
+    title: req.body.title,
+    post: req.body.post,
   };
-  const newBlog= new Blogs(newPost);
-  newBlog.save();   //saving new document to DB
+  const newBlog = new Blogs(newPost);
+  newBlog.save(); //saving new document to DB
   allPost.push(newPost);
   res.redirect("/");
-})
-
+});
 
 //listening
 app.listen(3000, function () {
   console.log("Server started at http://localhost:3000");
 });
-
